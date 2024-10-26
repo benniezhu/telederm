c3fb53630042a7258adccddf7345deeb32d8d4fd
benniezhu <bennie@benniedesktop>
2024-10-25 21:16:31 -0400

    more integration of 2022 data

--- code/clink_and_merge.R
+++ code/clink_and_merge.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(haven)
 
 clink <- list()
-for (year in 2020:2021){
+for (year in 2020:2022){
   file_name <- paste0('F:/data/MEPS/clink/clink', year, '.dta')
   df <- read_dta(file_name)
   clink[[year]] <- df
@@ -39,14 +39,29 @@ derm_visits <- read_dta("F:/projects/telederm/data/derm_visits.dta") %>%
   mutate(count = 1, 
          domain = 1)
 
+summarized_derm_visits <- read_dta("F:/projects/telederm/data/summarized_derm_visits.dta")
 
-FYC_dfs <- read_dta("F:/projects/telederm/data/FYC_dfs.dta") %>% 
-  select(-VARSTR, -VARPSU, -PERWT20F, -PERWT21F, -perwt, -DUID, -PID, -PANEL)
+summarized_medical_conditions <- read_dta("F:/projects/telederm/data/medical_conditions_summary.dta")
+
+charlson_score <- read_dta("F:/projects/telederm/data/ml_ready/MEPS_2020_2022_charlson_score.dta")
+
+#you need to unselect the variables to save derm_fyc; aka the not summary dataframe but still need to think 
+#
+#FYC_dfs <- read_dta("F:/projects/telederm/data/FYC_dfs.dta") %>% 
+#  select(-VARSTR, -VARPSU, -PERWT20F, -PERWT21F, -PERWT22F, -perwt, -DUID, -PID, -PANEL)
+FYC_dfs <- read_dta("F:/projects/telederm/data/FYC_dfs.dta") 
 
 #merge datasets 
 
+#this is the dataset that has all of the visits joined with the FYC
 derm_FYC <- full_join(derm_visits, FYC_dfs, by = c('DUPERSID', 'year'))
 
+#this dataset has summary information on visits and medical conditions joined with FYC
+derm_summary_FYCs <- left_join(FYC_dfs, summarized_derm_visits, by = c('DUPERSID', 'year')) %>% 
+  left_join(summarized_medical_conditions, by = c('DUPERSID' , 'year')) %>% 
+  left_join(charlson_score, by = c('DUPERSID', 'year'))
 
 
 write_dta(derm_FYC, "F:/projects/telederm/data/ml_ready/derm_FYC.dta")
+
+write_dta(derm_summary_FYCs, 'F:/projects/telederm/data/ml_ready/derm_summary_FYC.dta')
